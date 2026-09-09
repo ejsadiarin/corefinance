4508ace45fead5659a6580ff109e5e31e50dc749
ejsadiarin <ejs@ejsadiarin.com>
2026-09-09 20:31:14 +0800

    fix: remove cors to fix duplicate Allow CORS headers

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 	"github.com/google/uuid"
 
 	"github.com/coder/websocket"
@@ -23,14 +22,6 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r.Use(requestIDMiddleware)
 	r.Use(slogMiddleware)
 	r.Use(middleware.Recoverer)
-
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-User-ID"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}))
 
 	r.Get("/", s.HelloWorldHandler)
 
